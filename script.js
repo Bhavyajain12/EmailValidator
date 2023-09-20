a0ee2c1f0bb4f187cd019bdcf39afb8e18e92c98
Bhavyajain12 <bhavyajain1210@gmail.com>
2023-09-20 18:41:30 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+let result = {
+
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "Jainbhav1210",
+    "email": "Jainbhav1210@gmail.com",
+    "score": 0.64,
+    "state": "deliverable",
+    "domain": "gmail.com",
+    "reason": "valid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": true,
+    "did_you_mean": "",
+    "format_valid": true
+}
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked")
+    resultCont.innerHTML = `<img  width="125px" src="loading.svg" alt="">`
+    let key = "ema_live_O7reEFg3dSBN2EiWvOyT8aI7xipJapNIigyGVttD"
+    let email = document.getElementById("username").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+
+    let str = ``
+    for (key of Object.keys(result)) {
+        if (result[key] !== "" && result[key] !== " ") {
+            str = str + `<div> ${key}: ${result[key]}</div>`
+        }
+    }
+    console.log(str);
+    resultCont.innerHTML = str;
+})
+
+
+
